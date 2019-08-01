Save/copy options/Fixes of various calculation errors
Save of objects added (handles pickle errors with the mda object)
Errors in load_NMR corrected (skipped 3rd experiment type)
Model application corrected
Update problems with model upon experiment list editing corrected

r_class/detectors.py
@@ -521,7 +521,7 @@ class detect(mdl.model):
             self.SVD[bond]['T']=T
             self.__r_info(bond,**kwargs)
             self.__r_auto={'Error':err,'Peaks':pks,'rho_z':self.__rho[bond]}
-            self.__r_norm(bond)
+            self.__r_norm(bond,**kwargs)
             if np.size(bonds)>0:
                 if 'NT' in kwargs: #We don't re-normalize the results of detectors obtained with r_target
                     kwargs.pop('NT')
@@ -593,6 +593,13 @@ class detect(mdl.model):
         if 'sort_rho' not in kwargs:
             kwargs['sort_rho']='n'
         self.__r_info(bond,**kwargs)
+    
+    
+    def _addS2(self,bond=None):
+        pass
+    
+    def _R2_ex_corr(self,bond=None):
+        pass
     
     def __r_norm(self,bond=None,**kwargs):
         "Applies equal-max or equal-integral normalization"

r_class/mdl_sens.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
+import copy
 import DynamicModels as dm
 import os
 os.chdir('../Struct')
@@ -171,7 +172,7 @@ class model(object):
             R0=R0[0]
         
             
-        return R,R0
+        return R.copy(),R0.copy()
         
     
     def _rho_effCSA(self,exp_num=None,mdl_num=0,bond=None):
@@ -217,7 +218,7 @@ class model(object):
             R0=R0[0]
         
             
-        return R,R0
+        return R.copy(),R0.copy()
     
     def __apply_mdl(self,tMdl,A):
         "tMdl is a list of correlation times in the model, and A the amplitudes"
@@ -266,15 +267,16 @@ class model(object):
 
         SZ1=[SZeff[0],np.prod(SZeff[1:])]
 
-        
+        """
+        The order parameter of the input model yields the fraction of the model correlation that does not
+        change the internal effective correlation time. 
+        """
         Reff=np.multiply(np.repeat(np.transpose([S2]),SZ1[1],axis=1),np.reshape(R,SZ1))
         ReffCSA=np.multiply(np.repeat(np.transpose([S2CSA]),SZ1[1],axis=1),np.reshape(RCSA,SZ1))
         Reff=np.reshape(Reff,SZeff)
         ReffCSA=np.reshape(ReffCSA,SZeff)
         
-#        Reff=np.reshape(np.dot(np.transpose([S2]),np.reshape(R,[1,np.prod(SZR)])),SZeff)
-#        ReffCSA=np.reshape(np.dot(np.transpose([S2CSA]),np.reshape(RCSA,[1,np.prod(SZR)])),SZeff)
-#        
+     
         R0=np.zeros(SZ0)
         R0CSA=np.zeros(SZ0)
         
@@ -287,7 +289,7 @@ class model(object):
             SZ1=[np.prod(SZeff[0:2]),SZeff[2]]
             R00=np.dot(M0,np.reshape(R,SZ1).T)
             R0CSA0=np.dot(M0,np.reshape(RCSA,SZ1).T)
-            
+                
             Reff0=np.reshape(np.dot(M,np.reshape(R,SZ1).T).T-np.transpose([R00]),[1,np.prod(SZeff)])
             ReffCSA0=np.reshape(np.dot(M,np.reshape(RCSA,SZ1).T).T-np.transpose([R0CSA0]),[1,np.prod(SZeff)])
 
@@ -305,11 +307,9 @@ class model(object):
                 ReffCSA+=np.reshape(np.multiply(np.repeat(np.transpose([A0]),np.prod(SZR)),ReffCSA0),SZeff)
                 R0CSA+=np.reshape(np.multiply(np.repeat(np.transpose([A0]),SZR[0]),R0CSA0),SZ0)
                 
-#                R0+=np.reshape(np.dot(np.transpose([A0]),[R00]),SZ0)
-#                A0=A[:,1,k]
-#                ReffCSA+=np.reshape(np.dot(np.transpose([A0]),ReffCSA0),SZeff)
-#                R0CSA+=np.reshape(np.dot(np.transpose([A0]),[R0CSA0]),SZ0)
-            
+        
+        Reff+=ReffCSA
+        R0+=R0CSA
         
         return Reff,R0,ReffCSA,R0CSA
         
@@ -364,18 +364,33 @@ class model(object):
         
         self.del_exp(n) #Delete the experiment to hide this operation from the user
         
-        return R
-    
-    def clear_stored(self):
-        """mdl_sens objects store results of model calculations. However, in 
-        some situations, this may take up too much memory. We can clear them 
-        with this function"""
-        self.__Reff=np.zeros([0,np.size(self.tc())])
-        self.__R0=np.zeros([0,1])
-        self.__mdlinfo=pd.DataFrame(index=self.retExper()+self.retSpinSys())
-        self.__tMdl=list()
-        self.__AMdl=list()
+        return R 
+
+    def _clear_stored(self,exp_num=None):
+        "Unfortunately, we only have methods to apply models to all experiments at once"
+        "This means a change in the experiment list requires recalculation of all models for all experiments"
+        "This function deletes all model calculations"
         
+
+        if exp_num is None:
+            for m in self.__Reff:
+                m=None
+            for m in self.__ReffCSA:
+                m=None
+            for m in self.__R0:
+                m=None
+            for m in self.__R0CSA:
+                m=None
+        else:    
+            for m in self.__Reff:
+                np.delete(m,exp_num,axis=0)
+            for m in self.__ReffCSA:
+                np.delete(m,exp_num,axis=0)
+            for m in self.__R0:
+                np.delete(m,exp_num)
+            for m in self.__R0CSA:
+                np.delete(m,exp_num)
+                
     def zeff(self,t,tau=None):
         if tau==None:
             return self.z()+np.log10(t)-np.log10(10**self.z()+t)
@@ -440,3 +455,26 @@ class model(object):
             if k in props:
                 for m in hdl:
                     getattr(m,'set_{}'.format(k))(kwargs.get(k))
+                    
+    def copy(self,type='deep'):
+        """
+        |
+        |Returns a copy of the object. Default is deep copy (all objects except the molecule object)
+        | obj = obj0.copy(type='deep')
+        |To also create a copy of the molecule object, set type='ddeep'
+        |To do a shallow copy, set type='shallow'
+        """
+        if type=='ddeep':
+            out=copy.deepcopy(self)
+        elif type!='deep':
+            out=copy.copy(self)
+        else:
+            mol=self.molecule
+            self.molecule=None
+            out=copy.deepcopy(self)
+            self.molecule=mol
+            out.molecule=mol
+            
+        return out
+        
+        

r_class/sens.py
@@ -16,7 +16,6 @@ import sens
 class rates(mdl.model):  
     def __init__(self,tc=None,**kwargs):
         
-        
         """Probably a better way to do this, but I need to identify which
         child of mdl_sens is which later. Using isinstance requires me to 
         import the children into mdl_sens, but also import mdl_sens into its
@@ -69,8 +68,7 @@ class rates(mdl.model):
     
         "We need to initialize self.info"
         self.info=None  
-        "Here we feed in all the information on the experiments"
-        self.new_exp(**kwargs)
+        
 
         "Initialize some storage for rate constant calculation"
 #        self.__R=np.zeros([0,np.size(self.__tc)])
@@ -78,6 +76,8 @@ class rates(mdl.model):
 #        self.__info=pd.DataFrame(index=self.__exper+self.__spinsys)
         
         super().__init__()
+        "Here we feed in all the information on the experiments"
+        self.new_exp(**kwargs)
     
     def new_exp(self,**kwargs):
         "Count how many experiments are given"
@@ -134,7 +134,9 @@ class rates(mdl.model):
             self.info=info
         else:
             self.info=pd.concat([self.info,info],axis=1,ignore_index=True)
-         
+            
+        self._clear_stored()
+        
 #%% Make sure inputs all are the correct type (numpy arrays)         
     "Function to make sure all inputs are arrays, and have the correct sizes"    
     def __cleanup(self,ne):
@@ -291,21 +293,30 @@ class rates(mdl.model):
 
 #%% Delete experiment
     def del_exp(self,exp_num):
-        exp_num=np.atleast_1d(exp_num)
-        for k in exp_num:
-            self.info=self.info.drop(k,axis=1)
-            del self.__R[k]
-            del self.__RCSA[k]
-          
-        self.info.set_axis(np.arange(np.size(self.info.axes[1])),axis=1,inplace=True)
+        
+        if np.size(exp_num)>1:
+            for m in exp_num:
+                self.del_exp(m)
+        else:
+            self.info=self.info.drop(exp_num,axis=1)
+            del self.__R[exp_num]
+            del self.__RCSA[exp_num]
                 
+            self.info.set_axis(np.arange(np.size(self.info.axes[1])),axis=1,inplace=True)
+            self._clear_stored(exp_num)
+            
 #%% Adjust a parameter
     "We can adjust all parameters of a given type, or just one with the experiment index"
-    def set_par(self,type,index=None,value=None):
-        if index is None:
+    def set_par(self,type,value,exp_num=None):
+        if exp_num is None:
             self.info.at[type,:]=value
+            self.__R[:]=[None]*len(self.__R)
         else:
-            self.info.at[type,index]=value
+            self.info.at[type,exp_num]=value
+            self.__R[exp_num]=None
+            self.__RCSA[exp_num]=None
+         
+        self._clear_stored()
 
 #%% Correlation time axes   
     "Return correlation times or log of correlation times"        
@@ -340,7 +351,7 @@ class rates(mdl.model):
 #                self.__info=pd.concat([self.__info,self.info.loc[:,exp_num[k]]],axis=1,ignore_index=True)
               
         
-        return R
+        return R.copy()
     
     def _rhoCSA(self,exp_num,bond=None):
         """Calculates relaxation due to CSA only. We need this function to 
@@ -385,7 +396,7 @@ class rates(mdl.model):
             if bond==-1 & self.molecule.vXY.shape[0]>0:
                 nb=self.molecule.vXY.shape[0]
                 R=np.repeat([R],nb,axis=0)
-        return R
+        return R.copy()
     
     
 #%% Plot the rate constant sensitivites